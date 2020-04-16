190d89a9b9edbc81f392ac81e86005f216181780
Ruediger Bachmann <ruediger.bachmann@sap.com>
2020-04-16 22:32:04 +0200

    routes and controllers

--- my-express-app/app.js
+++ my-express-app/app.js
@@ -2,15 +2,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var createError = require('http-errors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var checkRouter = require('./routes/check');
 
 var app = express();
 
 // view engine setup
- app.set('views', path.join(__dirname, 'views'));
- app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,5 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/check', checkRouter);
 
-module.exports = app; 
+module.exports = app;
---
+++ my-express-app/routes/check.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+
+
+
+
+router.get('/', function(req, res, next) {
+
+
+  
+    res.render('check', { title: 'Hey', message: 'Hello there!' })
+    next();
+ 
+  
+  });
+
+
+
+
+
+
+
+module.exports = router;
