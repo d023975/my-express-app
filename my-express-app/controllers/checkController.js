84080a19c1ddc35f28f6de45d05c47914883a64d
Ruediger Bachmann <ruediger.bachmann@sap.com>
2020-04-18 19:00:52 +0200

    build more (error) handlers

---
+++ my-express-app/controllers/checkController.js
@@ -0,0 +1,8 @@
+var Check = require('../models/check');
+
+exports.check_detail = function(req, res, next) {
+	//    res.send('NOT IMPLEMENTED: Check detail: ' + req.params.id);
+
+	res.render('check', { title: 'Hey', message: `Hello there! ${req.params.checkId} ` });
+	next();
+};
--- my-express-app/routes/check.js
+++ my-express-app/routes/check.js
@@ -1,24 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var checkController = require('../controllers/checkController');
 
+// router.get('/ca?h*eck/:checkId', function(req, res, next) {
+// 	res.render('check', { title: 'Hey', message: 'Hello there!' });
+// 	next();
+// });
 
-
-
-
-router.get('/', function(req, res, next) {
-
-
-  
-    res.render('check', { title: 'Hey', message: 'Hello there!' })
-    next();
- 
-  
-  });
-
-
-
-
-
-
+router.get('/ca?h*eck/:checkId', checkController.check_detail);
 
 module.exports = router;
